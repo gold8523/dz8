fa8e4905d22ea168c138d2b37c0b153561641844
Denis Golodkov <gold8523@gmail.com>
2016-12-16 23:47:46 +0300

    commit 7 vk

--- vk_api.php
+++ vk_api.php
@@ -5,7 +5,7 @@ $token = '41a6032dab484dfd6b9ba2bcd3d3a7957ad11b1a4c5806ab3a8128eb6accbbdbfd1013
 //https://oauth.vk.com/authorize?client_id=5780232&display=page&redirect_uri=http://dz8/vk_api.php&scope=photos&response_type=token&v=5.60
 
 
-$upl = file_get_contents('https://api.vk.com/method/photos.getWallUploadServer?access_token=41a6032dab484dfd6b9ba2bcd3d3a7957ad11b1a4c5806ab3a8128eb6accbbdbfd1013df07dc7bc26f9e3s://api.vk.com/method/photos.getUploadServer?album_id=239145223&access_token=41a6032dab484dfd6b9ba2bcd3d3a7957ad11b1a4c5806ab3a8128eb6accbbdbfd1013df07dc7bc26f9e3');
+$upl = file_get_contents('https://api.vk.com/method/photos.getWallUploadServer?owner_id=2696830&access_token=41a6032dab484dfd6b9ba2bcd3d3a7957ad11b1a4c5806ab3a8128eb6accbbdbfd1013df07dc7bc26f9e3s://api.vk.com/method/photos.getUploadServer?album_id=239145223&access_token=41a6032dab484dfd6b9ba2bcd3d3a7957ad11b1a4c5806ab3a8128eb6accbbdbfd1013df07dc7bc26f9e3');
 $upl = json_decode($upl);
 
 foreach ($upl as $value) {
@@ -13,19 +13,16 @@ foreach ($upl as $value) {
         $vk [] = $item;
     }
 }
+print_r($vk[0]);
+$ch = curl_init($vk[0]);
 
-$ch = curl_init();
-curl_setopt($ch, CURLOPT_URL, $vk[0]);
-curl_setopt($ch, CURLOPT_HEADER, 0);
-curl_setopt($ch, CURLOPT_VERBOSE, 0);
+curl_setopt($ch, CURLOPT_POST, true);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-curl_setopt($ch, CURLOPT_POST,
-$file = [
-//    'photo' => "@" . '/photos1/pes.jpg'
-];
+$cfile = curl_file_create('/photos1', 'image/jpg', 'pes.jpg');
 
-curl_setopt($ch, CURLOPT_POSTFIELDS, $file);
+curl_setopt($ch, CURLOPT_POSTFIELDS, array( 'photo' => $cfile));
+
 $response = curl_exec($ch);
 $ph = json_decode($response);
-print_r($ph);
+var_dump($ph);
 //include 'vk.php';
