5c770d3c2b8b311d5ac00ba5f8795f75d764e7e7
Denis Golodkov <gold8523@gmail.com>
2016-12-17 19:50:15 +0300

    commit 9 vk foto

--- vk_api.php
+++ vk_api.php
@@ -8,21 +8,33 @@ $token = '41a6032dab484dfd6b9ba2bcd3d3a7957ad11b1a4c5806ab3a8128eb6accbbdbfd1013
 $upl = file_get_contents('https://api.vk.com/method/photos.getWallUploadServer?owner_id=2696830&access_token=41a6032dab484dfd6b9ba2bcd3d3a7957ad11b1a4c5806ab3a8128eb6accbbdbfd1013df07dc7bc26f9e3s://api.vk.com/method/photos.getUploadServer?album_id=239145223&access_token=41a6032dab484dfd6b9ba2bcd3d3a7957ad11b1a4c5806ab3a8128eb6accbbdbfd1013df07dc7bc26f9e3');
 $upl = json_decode($upl);
 
-foreach ($upl as $value) {
-    foreach ($value as $item) {
-        $vk [] = $item;
-    }
-}
-print_r($vk[0]);
-$ch = curl_init($vk[0]);
+$url = $upl->response->upload_url;
 
+
+$fname = dirname(__FILE__) . '/photos1/rif.jpg';
+$cfile = new CURLFile($fname, 'image/jpg');
+$file = [
+    'photo' => $cfile
+];
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_URL, $url);
 curl_setopt($ch, CURLOPT_POST, true);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-$cfile = curl_file_create('photos1/', 'image/jpg', 'pes.jpg');
+curl_setopt($ch, CURLOPT_HTTPHEADER,array('Content-Type: multipart/form-data'));
+//$cfile = curl_file_create('photos1/pes.jpg', 'image/jpg', 'pes.jpg');
+curl_setopt($ch, CURLOPT_POSTFIELDS, $file);
 
-curl_setopt($ch, CURLOPT_POSTFIELDS, array( 'photo' => $cfile));
+$result = curl_exec ($ch);
 
-$response = curl_exec($ch);
-$ph = json_decode($response);
-var_dump($ph);
-//include 'vk.php';
+if ($result === FALSE) {
+    echo "Error sending " . $fname .  " " . curl_error($ch);
+    curl_close ($ch);
+}else{
+    curl_close ($ch);
+    echo  "Result: " . $result;
+}
+
+//$ph = json_decode($result);
+//print_r($ph->photo);
+//$a = stripslashes($ph->photo);
+//print_r($a);
