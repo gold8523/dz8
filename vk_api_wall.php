10c7a81f0f2e3909c8d7d1749027adb48b6b77e9
Denis Golodkov <gold8523@gmail.com>
2016-12-20 17:01:26 +0300

    commit 15 vk картинка помещается на стену

--- vk_api_wall.php
+++ vk_api_wall.php
@@ -1,16 +1,20 @@
 <?php
 
-$token = '421771ec8254fc088e652ba4aeb56ccfb7abc3b3317bb120954b90f3c6cfc7653c0b4bfe3760bc960c668';
+$token = 'c27cf017c68d3dbb548716991d9a561b1ec84f6c420d1972c78b426ca49ba1dd0c27a48e8877fb13262bc';
+//https://oauth.vk.com/authorize?client_id=5780232&display=page&redirect_uri=http://oauth.vk.com/blank.html&scope=friends,photos,wall&response_type=token
 
-//https://oauth.vk.com/authorize?client_id=5780232&display=page&redirect_uri=http://dz8/vkform.php&scope=friends,photos,wall&response_type=token
-
-
+if (!empty($_FILES['image']['name'])) {
+    if ($_FILES['image']['type'] != "image/gif" && $_FILES['image']['type'] != "image/jpeg"
+        && $_FILES['image']['type'] != "image/png"
+    ) {
+        echo 'Выберете изображение формата jpeg, png или gif.';
+    }
+}
+$image = $_FILES['image'];
+$user = $_POST['user_id'];
 $upl = file_get_contents('https://api.vk.com/method/photos.getWallUploadServer?access_token=' . $token . '&v=5.60');
 $upl = json_decode($upl);
 $url = $upl->response->upload_url;
-
-
-
 
 $fname = $image['tmp_name'];
 $cfile = new CURLFile($fname, 'image/jpg', 'ozero.jpg');
@@ -26,13 +30,13 @@ curl_setopt($ch, CURLOPT_POSTFIELDS, $file);
 
 $result = curl_exec ($ch);
 
-if ($result === FALSE) {
-    echo "Error sending " . $fname .  " " . curl_error($ch);
-    curl_close ($ch);
-}else{
-    curl_close ($ch);
-    echo  "Result: " . $result;
-}
+//if ($result === FALSE) {
+//    echo "Error sending " . $fname .  " " . curl_error($ch);
+//    curl_close ($ch);
+//}else{
+//    curl_close ($ch);
+//    echo  "Result: " . $result;
+//}
 
 $photo = json_decode($result);
 
@@ -41,8 +45,12 @@ $photos = stripslashes($photo->photo);
 $vksave = file_get_contents('https://api.vk.com/method/photos.saveWallPhoto?photo=' . $photos . '&server=' . $photo->server . '&hash=' . $photo->hash . '&access_token=' . $token);
 
 $post_photo = json_decode($vksave);
-echo "<br><pre>";
-var_dump($post_photo);
-echo "<br>";
-$post_wall = file_get_contents('https://api.vk.com/method/wall.post?owner_id=' . $post_photo->response[0]->owner_id . '&access_token=' . $token . '&attachments=' . $post_photo->response[0]->id);
-var_dump($post_wall);
+
+$post_wall = file_get_contents('https://api.vk.com/method/wall.post?owner_id=' . $user . '&access_token=' . $token . '&attachments=' . $post_photo->response[0]->id);
+$post_wall = json_decode($post_wall);
+
+if ($post_wall->response->post_id > 0) {
+    echo "Изображение ушпешно опубликовано: id поста " . $post_wall->response->post_id;
+} else {
+    echo 'Ошибка укажите id пользователя';
+}
